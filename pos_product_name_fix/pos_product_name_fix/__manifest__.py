8c93b54716f14ff1910ac2fb267eeeda01d998dc
angkanacha50-cmd <angkanacha50@gmail.com>
2026-06-01 18:13:38 +0700

    Create __manifest__.py

---
+++ pos_product_name_fix/pos_product_name_fix/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'POS Product Name Fix',
+    'version': '1.0',
+    'depends': ['point_of_sale'],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_product_name_fix/static/src/css/pos_fix.css',
+        ],
+    },
+}
